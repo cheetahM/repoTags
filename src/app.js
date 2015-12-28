66b6708645a1b892cf84ba3b163036ea5f48ec5e
cmeshram <meshram.chetan@gmail.com>
2015-12-27 23:32:43 -0800

    adding a global app object using ampersand-app module

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import Router from './router'
 import styles from './styles/main.scss'
+import app from 'ampersand-app'
 
-window.app = {
+window.app = app
+app.extend ({
   init() {
     this.router = new Router()
     this.router.history.start()
   }
-}
+})
 
-window.app.init()
+app.on('all', function (event, payload) {
+  console.log('got local click', arguments)
+})
+
+app.init()
